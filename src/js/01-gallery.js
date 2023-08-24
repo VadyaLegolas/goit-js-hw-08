d3658bce70136d61a8d1a7bce95c37aadac7db30
Vlad <vadya-85@mail.ru>
2023-08-24 11:42:24 +0200

    fix task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import galleryTpl from '../templates/gallery.hbs';
 
 import { addLazySizesScript } from '../helpers/addLazySizesScript';
 import { addSrcToLazyImages } from '../helpers/addSrcToLazyImages';
@@ -13,7 +14,7 @@ const refs = {
   galleryUl: document.querySelector('.gallery'),
 };
 
-const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryMarkup = galleryTpl(galleryItems);
 refs.galleryUl.insertAdjacentHTML('beforeend', galleryMarkup);
 
 if ('loading' in HTMLImageElement.prototype) {
@@ -24,26 +25,9 @@ if ('loading' in HTMLImageElement.prototype) {
   addLazySizesScript();
 }
 
-function createGalleryMarkup(gallery) {
-  return gallery
-    .map(({ preview, original, description }) => {
-      return `
-        <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img loading="lazy" class="gallery__image lazyload" data-src="${preview}" alt="${description}" />
-            </a>
-        </li>`;
-    })
-    .join('');
-}
-
 let gallery = new SimpleLightbox('.gallery a', {
   captions: true,
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
-
-
-
-
