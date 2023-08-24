bc9f94bc8931005fedc17d4e96e06e9a630f0686
Vlad <vadya-85@mail.ru>
2023-08-24 09:54:28 +0200

    add task3

--- src/common.js
+++ src/common.js
@@ -1,5 +1,6 @@
 const common = {
   VIDEOPLAYER_CURRENT_TIME: 'videoplayer-current-time',
+  FEEDBACK_FORM_STATE: 'feedback-form-state',
 };
 
 export { common };
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import { common } from '../common';
+import trottle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
+  submitBtn: document.querySelector('button[type="submit"]'),
+};
+
+const formData =
+  JSON.parse(localStorage.getItem(common.FEEDBACK_FORM_STATE)) ?? {};
+
+refs.form.addEventListener('input', trottle(onFormInput, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+populateFormInput();
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(common.FEEDBACK_FORM_STATE, JSON.stringify(formData));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(
+    'FormData: ',
+    JSON.parse(localStorage.getItem(common.FEEDBACK_FORM_STATE))
+  );
+  e.currentTarget.reset();
+  localStorage.removeItem(common.FEEDBACK_FORM_STATE);
+}
+
+function populateFormInput() {
+  if (formData.email) {
+    refs.email.value = formData.email;
+  }
+  if (formData.message) {
+    refs.message.value = formData.message;
+  }
+}
