d7c34395084914e04ebd9f0a2ed0bbedc4a4784a
Vlad <vadya-85@mail.ru>
2023-08-24 07:47:23 +0200

    add task2

---
+++ src/common.js
@@ -0,0 +1,5 @@
+const common = {
+  VIDEOPLAYER_CURRENT_TIME: 'videoplayer-current-time',
+};
+
+export { common };
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
+import { common } from '../common';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let currentTime =
+  JSON.parse(localStorage.getItem(common.VIDEOPLAYER_CURRENT_TIME)) ?? '';
+
+player.on('timeupdate', trottle(getTime, 1000));
+
+player.on('play', function () {
+  player.setCurrentTime(Number(currentTime));
+});
+
+function getTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(
+      common.VIDEOPLAYER_CURRENT_TIME,
+      JSON.stringify(seconds)
+    );
+  });
+}
