68952f7d0fb059f8f5d2aab0049edec342aad96b
Vlad <vadya-85@mail.ru>
2023-08-25 09:35:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,12 @@ import { common } from '../common';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', () => player.setCurrentTime(Number(JSON.parse(localStorage.getItem(common.VIDEOPLAYER_CURRENT_TIME)) ?? 0)));
+// player.on('play', () => player.setCurrentTime(Number(JSON.parse(localStorage.getItem(common.VIDEOPLAYER_CURRENT_TIME)) ?? 0)));
+
+player.ready().then(function() {
+  // the player is ready
+  player.setCurrentTime(Number(JSON.parse(localStorage.getItem(common.VIDEOPLAYER_CURRENT_TIME)) ?? 0))
+});
 
 player.on('timeupdate', trottle(getTime, 1000));
 
